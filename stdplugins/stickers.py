fee5fad41a1d8d11ba085ea2bdacbebbe2d61969
Angga R. Saputra <kiritoclasher@gmail.com>
2020-01-20 03:53:42 +0700

    Update stickers.py

--- stdplugins/stickers.py
+++ stdplugins/stickers.py
@@ -29,7 +29,7 @@ from telethon.tl.types import (
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd(pattern="kangsticker ?(.*)"))
+@borg.on(admin_cmd(pattern="kang ?(.*)"))
 async def _(event):
     if event.fwd_from:
         return
@@ -45,7 +45,7 @@ async def _(event):
     me = borg.me
     userid = event.from_id
     packname = f"@UghteaSlayer's Sticker Pack"
-    packshortname = f"UghteaSlayee_{userid}"  # format: Uni_Borg_userid
+    packshortname = f"UghteaSlayer_{userid}"  # format: Uni_Borg_userid
 
     is_a_s = is_it_animated_sticker(reply_message)
     file_ext_ns_ion = "Sticker.png"
