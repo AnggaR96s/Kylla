2051c59d9f91a2ae924d69df3c3c20c6d8540bc0
Angga R. Saputra <kiritoclasher@gmail.com>
2020-01-21 03:07:39 +0700

    core: english-ify
    BSDK Dunno Hindi lang.

--- uniborg/_core.py
+++ uniborg/_core.py
@@ -20,14 +20,14 @@ async def load_reload(event):
         if shortname in borg._plugins:  # pylint:disable=E0602
             borg.remove_plugin(shortname)  # pylint:disable=E0602
         borg.load_plugin(shortname)  # pylint:disable=E0602
-        msg = await event.respond(f"BetiChod Successfully (re)loda pligon {shortname}")
+        msg = await event.respond(f"Successfully (re)loaded plugin {shortname}")
         await asyncio.sleep(DELETE_TIMEOUT)
         await msg.delete()
     except Exception as e:  # pylint:disable=C0103,W0703
         trace_back = traceback.format_exc()
         # pylint:disable=E0602
         logger.warn(f"Failed to (re)load plugin {shortname}: {trace_back}")
-        await event.respond(f"Failed to (re)loda pligon {shortname}: {e}")
+        await event.respond(f"Failed to (re)load plugin {shortname}: {e}")
 
 
 @borg.on(util.admin_cmd(pattern="(?:unload|remove) (?P<shortname>\w+)$"))  # pylint:disable=E0602
@@ -38,9 +38,9 @@ async def remove(event):
         msg = await event.respond(f"Not removing {shortname}")
     elif shortname in borg._plugins:  # pylint:disable=E0602
         borg.remove_plugin(shortname)  # pylint:disable=E0602
-        msg = await event.respond(f"Removed pligon {shortname}")
+        msg = await event.respond(f"Removed plugin {shortname}")
     else:
-        msg = await event.respond(f"Pligon {shortname} is not loda...")
+        msg = await event.respond(f"Plugin {shortname} is not loaded")
     await asyncio.sleep(DELETE_TIMEOUT)
     await msg.delete()
 
@@ -62,7 +62,7 @@ async def send_plug_in(event):
     )
     end = datetime.now()
     time_taken_in_ms = (end - start).seconds
-    await event.edit("BetiChod is pligon ko upload kar diya {} in {} seconds".format(input_str, time_taken_in_ms))
+    await event.edit("Uploaded {} in {} seconds".format(input_str, time_taken_in_ms))
     await asyncio.sleep(DELETE_TIMEOUT)
     await event.delete()
 
@@ -73,16 +73,16 @@ async def install_plug_in(event):
         return
     if event.reply_to_msg_id:
         try:
-            downloaded_file_name = await event.client.download_media(  # pylint:disable=E0602
+            downloaded_file_name = await event.client.download_media(
                 await event.get_reply_message(),
-                borg._plugin_path  # pylint:disable=E0602
+                borg.n_plugin_path  # pylint:disable=E0602
             )
             if "(" not in downloaded_file_name:
                 borg.load_plugin_from_file(downloaded_file_name)  # pylint:disable=E0602
-                await event.edit("Gandu, Instulled Pligon `{}`".format(os.path.basename(downloaded_file_name)))
+                await event.edit("Installed Plugin `{}`".format(os.path.basename(downloaded_file_name)))
             else:
                 os.remove(downloaded_file_name)
-                await event.edit("oh! Mkc, pligon instull na hove.")
+                await event.edit("Errors! Cannot install this plugin.")
         except Exception as e:  # pylint:disable=C0103,W0703
             await event.edit(str(e))
             os.remove(downloaded_file_name)
