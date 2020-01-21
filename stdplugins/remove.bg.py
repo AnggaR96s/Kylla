2bb3bacf2fd2e8104f11fc17105cdb80bf63b126
Angga R. Saputra <kiritoclasher@gmail.com>
2020-01-21 12:18:14 +0700

    rbg: change string

--- stdplugins/remove.bg.py
+++ stdplugins/remove.bg.py
@@ -39,7 +39,7 @@ async def _(event):
         message_id = event.reply_to_msg_id
         reply_message = await event.get_reply_message()
         # check if media message
-        await event.edit("`Abe Kuch Der Rukja ...`")
+        await event.edit("`Processing ...`")
         try:
             downloaded_file_name = await borg.download_media(
                 reply_message,
@@ -61,7 +61,7 @@ async def _(event):
     contentType = output_file_name.headers.get("content-type")
     if "image" in contentType:
         with io.BytesIO(output_file_name.content) as remove_bg_image:
-            remove_bg_image.name = "@PepeBot_ReMove.png"
+            remove_bg_image.name = "@Remove.png"
             await borg.send_file(
                 event.chat_id,
                 remove_bg_image,
@@ -72,7 +72,7 @@ async def _(event):
             )
         end = datetime.now()
         ms = (end - start).seconds
-        await event.edit("Background Removed in {} seconds using ReMove.BG API, powered by @PepeBot".format(ms))
+        await event.edit("Background Removed in {} seconds using ReMove.BG API".format(ms))
     else:
         await event.edit("ReMove.BG API returned Errors. Please report to @UniBorg\n`{}".format(output_file_name.content.decode("UTF-8")))
 
