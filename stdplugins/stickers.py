058e51ba0627e5371fbd4c35ee9d97afcad529a7
G.// <kiritoclasher@gmail.com>
2020-01-21 17:15:17 +0700

    Improvements

--- stdplugins/stickers.py
+++ stdplugins/stickers.py
@@ -44,8 +44,8 @@ async def _(event):
 
     me = borg.me
     userid = event.from_id
-    packname = f"@UghteaSlayer's Sticker Pack"
-    packshortname = f"UghteaSlayer_{userid}"  # format: Uni_Borg_userid
+    packname = f"@UniBorg's Stickers Pack"
+    packshortname = f"KyllaPack_{userid}"  # format: Uni_Borg_userid
 
     is_a_s = is_it_animated_sticker(reply_message)
     file_ext_ns_ion = "Sticker.png"
@@ -54,11 +54,11 @@ async def _(event):
     if is_a_s:
         file_ext_ns_ion = "AnimatedSticker.tgs"
         uploaded_sticker = await borg.upload_file(file, file_name=file_ext_ns_ion)
-        packname = f"@UghteaSlayer's @AnimatedStickerPack"
-        if userid == 719877937:
-            packshortname = "SnapDragon_Animated"
+        packname = f"@UniBorg's @AnimatedStickersPack"
+        if userid == 424935819:
+            packshortname = "Kylla_Animated"
         else:
-            packshortname = f"UghteaSlayer_{userid}_as" # format: Uni_Borg_userid
+            packshortname = f"Kylla_{userid}_as" # format: Uni_Borg_userid
     elif not is_message_image(reply_message):
         await event.edit("Invalid message type")
         return
